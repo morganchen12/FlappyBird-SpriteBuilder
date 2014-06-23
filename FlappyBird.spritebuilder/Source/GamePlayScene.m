a652e4b8ff58c02e6d981d59d472c3ffa40e8b5e
Morgan Chen <morganchen@Morgans-MacBook-Air.local>
2014-06-23 11:51:31 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -9,10 +9,17 @@
     // your code here
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
+    [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
+    timeSinceObstacle += delta;
+    if(timeSinceObstacle > 2.0f){
+        [self addObstacle];
+        timeSinceObstacle = 0.0f;
+    }
     // put update code here
 }
 
